dc8ff1948aba0c1ff7a984f8e952392e200a0b1a
Rapciu <mrapczewski711@gmail.com>
2019-08-22 17:08:53 +0200

    Redesign levels 1, 5, 6 Add the option of spinning and make a new sprite for the block

--- Breakout Clone/Assets/Scripts/Block Breaker Clone/Block.cs
+++ Breakout Clone/Assets/Scripts/Block Breaker Clone/Block.cs
@@ -8,11 +8,17 @@ public class Block : MonoBehaviour
     [SerializeField] GameObject level;
 
     [SerializeField] bool breakable = true;
+    [SerializeField] bool spin = false;
+    [Range(1f, 360f)] [SerializeField] float spinSpeed = 1f;
 
     [SerializeField] AudioClip[] breakSounds;
 
     GameManager gameManagerComp;
     Level levelComp;
+
+    Rigidbody2D rb;
+
+    float angle;
 
     private void DestroyBlock()
     {
@@ -29,10 +35,33 @@ public class Block : MonoBehaviour
         Destroy(gameObject);
     }
 
+    private void Spin()
+    {
+        if (spin)
+        {
+            if (rb.bodyType != RigidbodyType2D.Kinematic)
+            {
+                rb.bodyType = RigidbodyType2D.Kinematic;
+            }
+
+            rb.MoveRotation(angle);
+            angle += spinSpeed;
+        }
+    }
+
     private void Start()
     {
         gameManagerComp = FindObjectOfType<GameManager>();
         levelComp = level.GetComponent<Level>();
+
+        rb = GetComponent<Rigidbody2D>();
+
+        angle = Random.Range(0, 90);
+    }
+
+    private void Update()
+    {
+        Spin();
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
